8a71cfdd49a20bcb60bc26a70c09019dfa8dcfb8
Javier <63824485+CJJULIA1971@users.noreply.github.com>
2026-08-31 08:58:16 -0300

    Add files via upload

---
+++ Models/CartItem.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace AppSoftConsola.Models
+{
+    public class CartItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Producto Product { get; set; }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged(nameof(Quantity));
+                    UpdateSubtotal();
+                }
+            }
+        }
+
+        private decimal _subTotal;
+        public decimal SubTotal
+        {
+            get => _subTotal;
+            set
+            {
+                if (_subTotal != value)
+                {
+                    _subTotal = value;
+                    OnPropertyChanged(nameof(SubTotal));
+                }
+            }
+        }
+
+        public CartItem(Producto p, int qty)
+        {
+            Product = p;
+            Quantity = qty;
+            UpdateSubtotal();
+        }
+
+        private void UpdateSubtotal()
+        {
+            SubTotal = Quantity * Product.Amount;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿namespace AppSoftConsola.Models
+{
+    public class Producto
+    {
+        public int Code { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public int OrderShow { get; set; }
+
+        public Producto(int code, string desc, decimal amount, int orderShow = 0)
+        {
+            Code = code;
+            Description = desc;
+            Amount = amount;
+            OrderShow = orderShow;
+        }
+    }
+}
