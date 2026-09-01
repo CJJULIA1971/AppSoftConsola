e6ba825107cbc1e5232c8a5496ed48e728c00df8
Javier <63824485+CJJULIA1971@users.noreply.github.com>
2026-09-01 13:12:54 -0300

    Add files via upload

---
+++ Services/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SQLite;
+using AppSoftConsola.Models;
+
+namespace AppSoftConsola.Services
+{
+    public static class AuthService
+    {
+        private static string dbPath = "Data/pos10.db";
+
+        public static User GetUserById(string userId)
+        {
+            using (var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+            {
+                conn.Open();
+
+                string query = @"SELECT UserId, PasswordHash, RoleHash, Nombre, Apellido
+                         FROM TblUsers
+                         WHERE UserId = @id";
+
+                using (var cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", userId);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new User
+                            {
+                                UserId = reader["UserId"].ToString(),
+                                PasswordHash = reader["PasswordHash"].ToString(),
+                                RoleHash = reader["RoleHash"].ToString(),
+                                Nombre = reader["Nombre"].ToString(),
+                                Apellido = reader["Apellido"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Services/HashService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AppSoftConsola.Services
+{
+    public static class HashService
+    {
+        public static string Hash(string input)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
